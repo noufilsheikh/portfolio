078386c409c3c7e640cc44bb522e34fa5f35f27c
glitxherrr <glitxherrr@gmail.com>
2022-12-08 00:34:28 +0530

    Deploymet v2

--- src/components/logoAnimation/Logo.jsx
+++ src/components/logoAnimation/Logo.jsx
@@ -1,48 +1,12 @@
 import React from 'react'
 import './logo.scss'
-import { DrawSVGPlugin } from 'gsap-trial/DrawSVGPlugin'
-import gsap from 'gsap'
 import LogoTitle from '../../assets/images/logo-node.png'
 
-const Logo = () => {
-  const bgRef = React.useRef()
-  const outLineRef = React.useRef()
-  const outLineRef2 = React.useRef()
-  const solidLogoRef = React.useRef()
-
-  React.useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-    setTimeout(() => {
-      gsap
-        .timeline()
-        .to(bgRef.current, {
-          duration: 1,
-          opacity: 1,
-        })
-        .from(outLineRef.current, {
-          drawSVG: 0,
-          duration: 4,
-        })
-        .from(outLineRef2.current, {
-          drawSVG: 0,
-          duration: 4,
-        })
-      gsap.fromTo(
-        solidLogoRef.current,
-        {
-          opacity: 0,
-        },
-        {
-          opacity: 0.065,
-          delay: 4,
-          duration: 10,
-        },
-      )
-    }, 4000)
-  }, [])
+function Logo () {
+  
 
   return (
-    <div className="logo-container switch__stroke-color" ref={bgRef}>
+    <div className="logo-container switch__stroke-color" >
       <img className = "imgHome"
               src={LogoTitle}/>
   </div>
