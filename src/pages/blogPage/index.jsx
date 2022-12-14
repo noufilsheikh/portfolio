fdb540ddb1e2cee9f8eb8eb22bb6893128761477
glitxherrr <glitxherrr@gmail.com>
2022-12-14 15:26:20 +0530

    Deployment v8.5

--- src/pages/blogPage/index.jsx
+++ src/pages/blogPage/index.jsx
@@ -1,28 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import { SinglePost } from '../../components'
+import React from 'react'
+import { useAuth0 } from "@auth0/auth0-react";
 import Fade from 'react-reveal/Fade'
-import { blogData } from '../../data/blogData'
-
+import { projects } from './indexProjectsProfile'
+import { Project } from '../../components'
+import { Blast } from '../../components'
+import { NavBar } from '../../components/navigation/desktop/nav-bar';
 import './blogpage.scss'
 
 const Blog = () => {
-  const [blog, setBlog] = useState([])
 
-  useEffect(() => {
-    setBlog(blogData)
-  }, [])
+
+  const { user } = useAuth0();
+
+  if (!user) {
+    return null;
+  }
 
   return (
-    <>
+    <div className="profileContainer">
+        
+    <div className="profile-grid">
+      <div className="profile__header">
+        <img
+          src={user.picture}
+          alt="Profile"
+          className="profile__avatar"
+        />
+        <div className="profile__headline">
+          <h2 className="profile__title">{user.name}</h2>
+          <span className="profile__description">{user.email}</span>
 
-        <section className="section__blogpage section__padding">
-          <div className="section__blogpage-wrapper">
-            {blog.map((postsItems, index) => {
-              return <SinglePost key={index} {...postsItems} />
-            })}
-          </div>
-        </section>
-    </>
+          <NavBar />
+
+
+      <div className="animationProfileContainer">
+        {projects.map((project, index) => {
+          return <Project {...project} key={index} />
+        })}
+      </div>
+
+      </div>
+      </div>
+     
+      
+      </div>
+    </div>
+
+
   )
 }
-export default Blog
+
+export default Blog;
